22bdbc7c019fc78cf69187d79601dd7adfcd562f
AV-MMI <villalobos.aaron@hotmail.com>
2024-04-29 21:31:33 -0400

    add modified card component from daisyUI: must fix the gap between the button of card actions, and the card menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import { Fragment, useRef, useState } from 'react'
+import { Card } from './components/Card.jsx';
 import './App.css'
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(true);
-  const theme = useRef((darkTheme ? "dark" : "light"));
 
   return (
-    <div className="content" data-theme={(darkTheme ? 'dark' : 'light')} onClick={()=> alert('dam bubling')}>
-      <nav className="">
-        <div className="">
-          <h1>Artify: Memory Gallery</h1>
-          <span> Thanks to <a href="">Artic</a></span>
+    <div className="relative content w-screen h-screen" data-theme={(darkTheme ? 'dark' : 'light')} >
+      <nav className="p-3 flex flex-row items-center justify-between bg-base-200 w-full">
+        <div className="flex flex-row items-center">
+          <h1 className="text-4xl">Artify: Memory Gallery</h1>
+          <span className="ml-3"> Thanks to <a href="https://www.artic.edu/" target="_blank">Artic</a></span>
         </div>
-        <button className="btn" onClick={(e) => {
+        <button className="btn btn-outline btn-primary" onClick={(e) => {
           e.stopPropagation();
           setDarkTheme(!darkTheme)}} >{(darkTheme ? 'Light Theme' : 'Dark Theme')}</button>
       </nav>
+      
+      <div className="p-4 min-h-4/6 w-5/6 bg-base-200 m-auto mt-14 flex justify-center items-center">
+          <div className="flex flex-col space-y-5">
+            <Card className="" size="w-52" />
+            <Card className="" size="w-52" />
+          </div>
+      </div>
+
+
+      <footer class="footer p-1 bg-base-300 flex justify-center absolute bottom-0">
+        <div>
+          <a href="https://github.com/AV-MMI" target="_blank"><img className="h-9"src={(darkTheme ? "./src/assets/github-mark-white.png" : "./src/assets/github-mark.png")} alt="github logo"/></a>
+        </div>
+      </footer> 
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+
+export { Card }
+
+function Card({size="w-60", author="jane", title="starry night", url}){
+    const cardMenuEl = useRef(null);
+    const toggleMenu = (e) => {
+
+        cardMenuEl.current.classList.toggle('hidden');
+        cardMenuEl.current.classList.toggle('max-h-fit')
+        e.target.classList.toggle('rounded-t-md')
+        e.target.classList.toggle('border-2')
+
+        e.target.textContent = (e.target.textContent == "see more" ? "see less" : "see more");
+
+        e.stopPropagation();
+    }
+
+    return (
+        <div className={`p-0 card-compact relative rounded-lg shadow-2lx border-2 border-neutral-300 ${size}`}>
+        <div className="card-actions absolute w-full rounded-lg h-fit flex  flex justify-center">
+            <div ref={cardMenuEl} className="flex flex-col justify-around w-full bg-base-300 rounded-t-lg max-h-0 overflow-hidden hidden transition duration-600 opacity-80">
+                <span>author: {author}</span>
+                <span>title: {title}</span>
+                <span><a href="" target="_blank">view in page</a></span>
+            </div>
+            <button class="btn btn-square btn-sm w-full bg-base-200 border-2 rounded-t-none rounded-t-md hover:border-2 border-b-neutral-300 opacity-90" onClick={toggleMenu}>
+                see more
+            </button>
+        </div>
+        <button className="p-0 w-full h-full rounded-lg">
+            <img className="w-full h-full rounded-lg" src="https://www.artic.edu/iiif/2/e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9/full/843,/0/default.jpg" alt="" />
+        </button>
+        </div>
+    )
+}
